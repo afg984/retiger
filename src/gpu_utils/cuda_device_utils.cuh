9a4f680a4bd921dc09e022a5991c0ac456835720
afg <afg984@gmail.com>
2018-05-03 15:48:15 +0800

    atomicAdd

--- src/gpu_utils/cuda_device_utils.cuh
+++ src/gpu_utils/cuda_device_utils.cuh
@@ -5,17 +5,9 @@
 #include "src/gpu_utils/cuda_settings.h"
 
 #ifdef CUDA_DOUBLE_PRECISION
-__device__ inline double cuda_atomic_add(double* address, double val)
+__device__ inline void cuda_atomic_add(double* address, double value)
 {
-	unsigned long long int* address_as_ull = (unsigned long long int*)address;
-	unsigned long long int old = *address_as_ull, assumed;
-	do
-	{
-		assumed = old;
-		old = atomicCAS(address_as_ull, assumed, __double_as_longlong(val + __longlong_as_double(assumed)));
-	}
-	while (assumed != old); // Note: uses integer comparison to avoid hang in case of NaN (since NaN != NaN)
-	return __longlong_as_double(old);
+  atomicAdd(address,value);
 }
 #else
 __device__ inline void cuda_atomic_add(float* address, float value)
